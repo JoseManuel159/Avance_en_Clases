c292caab8af568e1221fe69d0ff464e9b8007621
JoseManuel159 <134978812+JoseManuel159@users.noreply.github.com>
2024-11-03 19:29:32 -0500

    Add files via upload

--- src/main/java/pe/edu/upeu/sysalmacen/control/UnidadMedidaController.java
+++ src/main/java/pe/edu/upeu/sysalmacen/control/UnidadMedidaController.java
@@ -1,29 +1,53 @@
 package pe.edu.upeu.sysalmacen.control;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pe.edu.upeu.sysalmacen.dtos.CategoriaDTO;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pe.edu.upeu.sysalmacen.dtos.UnidadMedidaDTO;
 import pe.edu.upeu.sysalmacen.mappers.UnidadMedidaMapper;
 import pe.edu.upeu.sysalmacen.modelo.UnidadMedida;
 import pe.edu.upeu.sysalmacen.servicio.IUnidadMedidaService;
 
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/unidadmedida")
-@CrossOrigin("*")
+@RequestMapping("/unidadmedidas")
 public class UnidadMedidaController {
     private final IUnidadMedidaService unidadMedidaService;
     private final UnidadMedidaMapper unidadMedidaMapper;
+
     @GetMapping
     public ResponseEntity<List<UnidadMedidaDTO>> findAll() {
         List<UnidadMedidaDTO> list = unidadMedidaMapper.toDTOs(unidadMedidaService.findAll());
         return ResponseEntity.ok(list);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<UnidadMedidaDTO> findById(@PathVariable("id") Long id) {
+        UnidadMedida obj = unidadMedidaService.findById(id);
+        return ResponseEntity.ok(unidadMedidaMapper.toDTO(obj));
+    }
+    @PostMapping
+    public ResponseEntity<Void> save(@Valid @RequestBody UnidadMedidaDTO dto) {
+        UnidadMedida obj = unidadMedidaService.save(unidadMedidaMapper.toEntity(dto));
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdUnidad())
+                        .toUri();
+        return ResponseEntity.created(location).build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UnidadMedidaDTO> update(@Valid @PathVariable("id") Long id,
+                                                  @RequestBody UnidadMedidaDTO dto) {
+        dto.setIdUnidad(id);
+        UnidadMedida obj = unidadMedidaService.update(id, unidadMedidaMapper.toEntity(dto));
+        return ResponseEntity.ok(unidadMedidaMapper.toDTO(obj));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        unidadMedidaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
--- src/main/java/pe/edu/upeu/sysalmacen/mappers/UnidadMedidaMapper.java
+++ src/main/java/pe/edu/upeu/sysalmacen/mappers/UnidadMedidaMapper.java
@@ -3,6 +3,9 @@ package pe.edu.upeu.sysalmacen.mappers;
 import org.mapstruct.Mapper;
 import pe.edu.upeu.sysalmacen.dtos.UnidadMedidaDTO;
 import pe.edu.upeu.sysalmacen.modelo.UnidadMedida;
+
 @Mapper(componentModel = "spring")
-public interface UnidadMedidaMapper extends GenericMapper<UnidadMedidaDTO, UnidadMedida>{
+public interface UnidadMedidaMapper extends
+        GenericMapper<UnidadMedidaDTO, UnidadMedida>
+{
 }
